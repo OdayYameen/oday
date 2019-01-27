95e7bddba53b42f820a90464765952b6e14f150b
OdayYameen <47007641+OdayYameen@users.noreply.github.com>
2019-01-27 17:28:56 +0200

    MyCommite

---
+++ PrintName/src/MYclass.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class MYclass {
+
+	public static void main(String[] args) {
+		
+		String name=JOptionPane.showInputDialog("Enter Your Name pleas???");
+		JOptionPane.showMessageDialog(null, "Your Name is "+name);
+		
+
+	}
+
+}
